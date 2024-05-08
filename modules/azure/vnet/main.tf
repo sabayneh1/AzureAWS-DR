7bd8db8a0ccf852742e07d08ff7cdb7dafc44ea2
sam <sgoshime@gmail.com>
2024-05-08 15:00:00 -0400

    Added main.tf

--- modules/azure/vnet/main.tf
+++ modules/azure/vnet/main.tf
@@ -26,22 +26,5 @@ resource "azurerm_subnet" "public_c" {
   address_prefixes     = ["10.0.3.0/24"]
 }
 
-output "vnet_id" {
-  value = azurerm_virtual_network.main.id
-}
 
-output "subnet_ids" {
-  value = [azurerm_subnet.public_a.id, azurerm_subnet.public_b.id, azurerm_subnet.public_c.id]
-}
 
-output "public_subnet_a_id" {
-  value = azurerm_subnet.public_a.id
-}
-
-output "public_subnet_b_id" {
-  value = azurerm_subnet.public_b.id
-}
-
-output "public_subnet_c_id" {
-  value = azurerm_subnet.public_c.id
-}
