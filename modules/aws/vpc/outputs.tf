2f8991a51e5d7378516b33ea6711f2ce9c3bb8e7
sam <sgoshime@gmail.com>
2024-05-15 15:00:00 -0400

    Added outputs.tf

--- modules/aws/vpc/outputs.tf
+++ modules/aws/vpc/outputs.tf
@@ -6,6 +6,18 @@ output "subnet_ids" {
   value = [aws_subnet.public_a.id, aws_subnet.public_b.id, aws_subnet.public_c.id]
 }
 
+output "public_subnet_a_id" {
+  value = aws_subnet.public_a.id
+}
+
+output "public_subnet_b_id" {
+  value = aws_subnet.public_b.id
+}
+
+output "public_subnet_c_id" {
+  value = aws_subnet.public_c.id
+}
+
 output "security_group_id" {
   value = aws_security_group.my_sg.id
 }
