eb748689c0d0787306e3941a64f8a0539f8d8833
sam <sgoshime@gmail.com>
2024-05-16 09:00:00 -0400

    Added main.tf

--- modules/aws/vpc/main.tf
+++ modules/aws/vpc/main.tf
@@ -104,23 +104,3 @@ resource "aws_security_group" "my_sg" {
     Name = "my-sg"
   }
 }
-
-output "vpc_id" {
-  value = aws_vpc.main.id
-}
-
-output "public_subnet_a_id" {
-  value = aws_subnet.public_a.id
-}
-
-output "public_subnet_b_id" {
-  value = aws_subnet.public_b.id
-}
-
-output "public_subnet_c_id" {
-  value = aws_subnet.public_c.id
-}
-
-output "security_group_id" {
-  value = aws_security_group.my_sg.id
-}
