dda8c2954b193d04a9f77d2cb9998139b1670d47
sam <sgoshime@gmail.com>
2024-08-05 12:00:00 -0400

    Added variables.tf

---
+++ modules/aws/ec2/variables.tf
@@ -0,0 +1,14 @@
+variable "vpc_id" {
+  description = "The ID of the VPC"
+  type        = string
+}
+
+variable "security_group" {
+  description = "The security group to associate with the EC2 instances"
+  type        = string
+}
+
+variable "subnet_ids" {
+  description = "The list of subnet IDs to launch resources in"
+  type        = list(string)
+}
