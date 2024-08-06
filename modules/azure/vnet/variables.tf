5bc5c71ccd51802357e7b743eaa610beb7ebbf18
sam <sgoshime@gmail.com>
2024-08-06 08:00:00 -0400

    Added variables.tf

---
+++ modules/azure/vnet/variables.tf
@@ -0,0 +1,10 @@
+
+variable "azure_location" {
+  description = "The Azure location to deploy to"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group in which to create the resources"
+  type        = string
+}
