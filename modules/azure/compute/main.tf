243868b39f3e42be0da36bbfc5bbc2ef6b351a91
sam <sgoshime@gmail.com>
2024-08-06 09:00:00 -0400

    Added main.tf

---
+++ modules/azure/compute/main.tf
@@ -0,0 +1,51 @@
+resource "azurerm_network_interface" "main" {
+  name                = "main-nic"
+  location            = var.azure_location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = var.subnet_id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_virtual_machine" "main" {
+  name                  = "main-vm"
+  location              = var.azure_location
+  resource_group_name   = var.resource_group_name
+  network_interface_ids = [azurerm_network_interface.main.id]
+  vm_size               = "Standard_DS1_v2"
+
+  storage_os_disk {
+    name              = "osdisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "adminuser"
+    admin_password = "P@ssword1234!"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+
+  tags = {
+    environment = "testing"
+  }
+}
+
+output "compute_id" {
+  value = azurerm_virtual_machine.main.id
+}
