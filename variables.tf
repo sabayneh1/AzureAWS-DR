2941216209ac93e6c20cc4fc966015a051bdffe5
sam <sgoshime@gmail.com>
2024-08-06 19:00:00 -0400

    Added variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  description = "The AWS region to deploy to"
+  type        = string
+  default     = "ca-central-1"
+}
+
+variable "azure_location" {
+  description = "The Azure location to deploy to"
+  type        = string
+  default     = "Canada Central"
+}
