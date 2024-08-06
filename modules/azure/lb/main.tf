144ed7ec61883b36865ddbc772da1365daddd2ca
sam <sgoshime@gmail.com>
2024-08-06 12:00:00 -0400

    Added main.tf

---
+++ modules/azure/lb/main.tf
@@ -0,0 +1,48 @@
+resource "azurerm_lb" "main" {
+  name                = "main-lb"
+  location            = var.azure_location
+  resource_group_name = var.resource_group_name
+  sku                 = "Basic"
+
+  frontend_ip_configuration {
+    name                 = "publicIPAddress"
+    public_ip_address_id = azurerm_public_ip.main.id
+  }
+}
+
+resource "azurerm_public_ip" "main" {
+  name                = "main-pip"
+  location            = var.azure_location
+  resource_group_name = var.resource_group_name
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_lb_backend_address_pool" "bpe" {
+  loadbalancer_id = azurerm_lb.main.id
+  name            = "BackEndAddressPool"
+}
+
+resource "azurerm_lb_probe" "main" {
+  name                = "http_probe"
+  loadbalancer_id     = azurerm_lb.main.id
+  protocol            = "Http"
+  port                = 80
+  request_path        = "/"
+  interval_in_seconds = 5
+  number_of_probes    = 2
+}
+
+resource "azurerm_lb_rule" "main" {
+  name                           = "http_rule"
+  loadbalancer_id                = azurerm_lb.main.id
+  protocol                       = "Tcp"
+  frontend_port                  = 80
+  backend_port                   = 80
+  frontend_ip_configuration_name = "publicIPAddress"
+  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.bpe.id]
+  probe_id                       = azurerm_lb_probe.main.id
+}
+
+output "lb_backend_id" {
+  value = azurerm_lb_backend_address_pool.bpe.id
+}
