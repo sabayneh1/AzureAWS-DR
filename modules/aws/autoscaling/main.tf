31b4f3b4549b3e6e93091e91482586b581a85d76
sam <sgoshime@gmail.com>
2024-08-13 15:00:00 -0400

    Added main.tf

--- modules/aws/autoscaling/main.tf
+++ modules/aws/autoscaling/main.tf
@@ -1,13 +1,50 @@
-resource "aws_autoscaling_group" "my_asg" {
-  name                      = "my_asg"
-  max_size                  = 5
-  min_size                  = 2
-  health_check_type         = "ELB"
-  desired_capacity          = 2
-  target_group_arns         = [var.target_group_arn]
-  vpc_zone_identifier       = var.subnet_ids
+resource "random_string" "suffix" {
+  length  = 6
+  special = false
+}
 
-  launch_configuration = aws_launch_configuration.my_launch_configuration.name
+# Launch Template Resource
+resource "aws_launch_template" "my_launch_template" {
+  name_prefix   = "my-launch-template-${random_string.suffix.result}"
+  image_id      = "ami-0c9f6749650d5c0e3"
+  instance_type = "t2.micro"
+  key_name      = "ubuntu"
+
+  network_interfaces {
+    associate_public_ip_address = true
+    security_groups             = [var.security_group]
+  }
+
+  block_device_mappings {
+    device_name = "/dev/sda1"
+
+    ebs {
+      volume_size = 8
+      volume_type = "gp2"
+    }
+  }
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "my-asg-instance"
+    }
+  }
+}
+# Auto Scaling Group Resource
+resource "aws_autoscaling_group" "my_asg" {
+  name                = "my_asg"
+  max_size            = 5
+  min_size            = 2
+  health_check_type   = "ELB"
+  desired_capacity    = 2
+  target_group_arns   = [var.target_group_arn]
+  vpc_zone_identifier = var.subnet_ids
+
+  launch_template {
+    id      = aws_launch_template.my_launch_template.id
+    version = "$Latest"
+  }
 
   tag {
     key                 = "Name"
@@ -16,20 +53,7 @@ resource "aws_autoscaling_group" "my_asg" {
   }
 }
 
-resource "aws_launch_configuration" "my_launch_configuration" {
-  name          = "my-launch-configuration"
-  image_id      = "ami-06878d265978313ca"
-  instance_type = "t2.micro"
-  key_name      = "ubuntu"
-  security_groups = [var.security_group]
-
-
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
+# Scale Up Policy
 resource "aws_autoscaling_policy" "scale_up" {
   name                   = "scale_up"
   policy_type            = "SimpleScaling"
@@ -39,6 +63,7 @@ resource "aws_autoscaling_policy" "scale_up" {
   cooldown               = 300
 }
 
+# Scale Down Policy
 resource "aws_autoscaling_policy" "scale_down" {
   name                   = "scale_down"
   policy_type            = "SimpleScaling"
@@ -47,4 +72,3 @@ resource "aws_autoscaling_policy" "scale_down" {
   scaling_adjustment     = -1
   cooldown               = 300
 }
-
