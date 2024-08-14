285066ea0e3aa408d2713fcbc457de99856db7fc
sam <sgoshime@gmail.com>
2024-08-14 15:00:00 -0400

    Added outputs.tf

--- modules/azure/compute/outputs.tf
+++ modules/azure/compute/outputs.tf
@@ -1,3 +1,7 @@
-output "compute_id" {
-  value = azurerm_virtual_machine.main.id
+output "vmss_id" {
+  value = azurerm_virtual_machine_scale_set.main.id
+}
+
+output "azure_vmss_public_ips" {
+  value = azurerm_virtual_machine_scale_set.main.public_ip_address
 }
