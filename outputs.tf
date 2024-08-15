e82dedbdc7fbef7b91f44baf5f3a2d2c09b8790b
sam <sgoshime@gmail.com>
2024-08-15 13:02:52 -0400

    Added outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -17,3 +17,15 @@ output "azure_vnet_id" {
 output "azure_subnet_ids" {
   value = module.azure_vnet.subnet_ids
 }
+output "vmss_id" {
+  value = module.azure_compute_vmss.vmss_id
+}
+
+
+output "aws_public_ips" {
+  value = module.aws_ec2_a.public_ip
+}
+
+output "azure_vmss_ips" {
+  value = module.azure_compute_vmss.public_ip_address
+}
