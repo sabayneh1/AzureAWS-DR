1b04e976d086a0943139ca78a1723ee6f4a81246
sam <sgoshime@gmail.com>
2024-08-23 16:00:00 -0400

    Added variables.tf

--- modules/aws/autoscaling/variables.tf
+++ modules/aws/autoscaling/variables.tf
@@ -1,14 +1,20 @@
-variable "target_group_arn" {
-  description = "The ARN of the target group"
-  type        = string
-}
+# variable "target_group_arn" {
+#   description = "The ARN of the target group"
+#   type        = string
+# }
 
-variable "subnet_ids" {
-  description = "The list of subnet IDs"
-  type        = list(string)
-}
+# variable "subnet_ids" {
+#   description = "The list of subnet IDs"
+#   type        = list(string)
+# }
 
-variable "security_group" {
-  description = "The security group to associate with the instances"
-  type        = string
-}
+# variable "security_group" {
+#   description = "The security group to associate with the instances"
+#   type        = string
+# }
+
+# modules/aws/autoscaling/variables.tf
+variable "launch_template_id" {}
+variable "subnet_ids" {}
+variable "target_group_arn" {}
+
