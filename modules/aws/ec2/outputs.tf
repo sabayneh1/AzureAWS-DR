5f852b1e8cc87bef30fcc8e87fa43da18c3c9e27
sam <sgoshime@gmail.com>
2024-08-23 12:00:00 -0400

    Added outputs.tf

--- modules/aws/ec2/outputs.tf
+++ modules/aws/ec2/outputs.tf
@@ -1,6 +1,6 @@
 output "launch_template_id" {
   value = aws_launch_template.my_launch_template.id
 }
-output "aws_ec2_public_ips" {
-  value = [aws_instance.my_instance.public_ip]
-}
+# output "aws_instance_public_ip" {
+#   value = aws_instance.my_instance.*.public_ip
+# }
