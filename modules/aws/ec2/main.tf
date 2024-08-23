fd2faa5c46cc0617a251e5eb963c7ea2f8fded79
sam <sgoshime@gmail.com>
2024-08-23 11:00:00 -0400

    Added main.tf

--- modules/aws/ec2/main.tf
+++ modules/aws/ec2/main.tf
@@ -1,28 +1,23 @@
-resource "aws_launch_template" "my_launch_template" {
-  name_prefix   = "${var.launch_template_name}-"  # This will ensure uniqueness
-  image_id      = "ami-0c9f6749650d5c0e3"
-  instance_type = "t2.micro"
-  key_name      = "ubuntu"
 
-  
-  provisioner "local-exec" {
-    command = <<EOT
-      ansible-playbook -i '${self.public_ip},' -u ubuntu --private-key /home/sam/Multi-CloudDisasterRecovery/multi-Cloud-DR/ubuntu.pem /home/sam/Multi-CloudDisasterRecovery/multi-Cloud-DR/ansible/site.yml
-    EOT
-  }
+# modules/aws/ec2/main.tf
+resource "aws_launch_template" "my_launch_template" {
+  name_prefix   = "${var.launch_template_name}-"
+  image_id      = var.image_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
   block_device_mappings {
     device_name = "/dev/sda1"
 
     ebs {
-      volume_size = 8
-      volume_type = "gp2"
+      volume_size = var.volume_size
+      volume_type = var.volume_type
     }
   }
 
   network_interfaces {
     associate_public_ip_address = true
     security_groups             = [var.security_group]
-    network_interface_id = aws_launch_template.my_launch_template.id
-    device_index         = 0
+    device_index                = 0
   }
 }
