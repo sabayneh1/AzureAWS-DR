be12dc4cec582437e8ad1acc738d1a4403ba2fcc
sam <sgoshime@gmail.com>
2024-08-23 13:00:00 -0400

    Added variables.tf

--- modules/aws/ec2/variables.tf
+++ modules/aws/ec2/variables.tf
@@ -1,20 +1,28 @@
-variable "vpc_id" {
-  description = "The ID of the VPC"
-  type        = string
-}
+# variable "vpc_id" {
+#   description = "The ID of the VPC"
+#   type        = string
+# }
 
-variable "security_group" {
-  description = "The security group to associate with the EC2 instances"
-  type        = string
-}
+# variable "security_group" {
+#   description = "The security group to associate with the EC2 instances"
+#   type        = string
+# }
 
-variable "subnet_ids" {
-  description = "The list of subnet IDs to launch resources in"
-  type        = list(string)
-}
+# variable "subnet_ids" {
+#   description = "The list of subnet IDs to launch resources in"
+#   type        = list(string)
+# }
 
-variable "launch_template_name" {
-  description = "The name of the launch template"
-  type        = string
-}
+# variable "launch_template_name" {
+#   description = "The name of the launch template"
+#   type        = string
+# }
 
+# modules/aws/ec2/variables.tf
+variable "launch_template_name" {}
+variable "image_id" {}
+variable "instance_type" {}
+variable "key_name" {}
+variable "volume_size" {}
+variable "volume_type" {}
+variable "security_group" {}
