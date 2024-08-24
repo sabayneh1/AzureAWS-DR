4226ed83903c951f55e6c9a4ef7ce4e7932e997a
sam <sgoshime@gmail.com>
2024-08-24 10:00:00 -0400

    Added main.tf

--- modules/azure/vnet/main.tf
+++ modules/azure/vnet/main.tf
@@ -27,4 +27,73 @@ resource "azurerm_subnet" "public_c" {
 }
 
 
+# Define Network Security Group
+resource "azurerm_network_security_group" "main" {
+  name                = "main-nsg"
+  location            = var.azure_location
+  resource_group_name = var.resource_group_name
 
+  security_rule {
+    name                       = "allow_ssh"
+    priority                   = 1000
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "allow_http"
+    priority                   = 1010
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "allow_https"
+    priority                   = 1020
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "allow_all_outbound"
+    priority                   = 2000
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# Associate NSG with Subnets
+resource "azurerm_subnet_network_security_group_association" "public_a_nsg" {
+  subnet_id                 = azurerm_subnet.public_a.id
+  network_security_group_id = azurerm_network_security_group.main.id
+}
+
+resource "azurerm_subnet_network_security_group_association" "public_b_nsg" {
+  subnet_id                 = azurerm_subnet.public_b.id
+  network_security_group_id = azurerm_network_security_group.main.id
+}
+
+resource "azurerm_subnet_network_security_group_association" "public_c_nsg" {
+  subnet_id                 = azurerm_subnet.public_c.id
+  network_security_group_id = azurerm_network_security_group.main.id
+}
